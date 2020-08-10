0929a67272a4a8f5221a67b2bb6cdcb498ae4ec1
Kevin Sun <t-kevsun@microsoft.com>
2020-08-10 17:30:25 -0400

    update

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: ["Jarod", "Hannah", "Mitch", "Navy", "Ruslan"]
+        body: ["Jarod", "Hannah", "Kevin", "Mitch", "Navy", "Ruslan"]
     }
 };
 
