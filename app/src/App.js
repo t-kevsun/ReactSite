1eaf8c87d855d95066a336bd9cf0b48a45c400b4
Kevin Sun <t-kevsun@microsoft.com>
2020-08-12 11:38:20 -0400

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, lazy, Suspense } from 'react';
+import 'bulma/css/bulma.css';
+import './styles.scss';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { HeaderBar, NavBar, NotFound } from './components';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Look at this really cool spinny thing we have here. 1/28/2020 3:47PM
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Everything is fine.
-        </a>
-        <a 
-          className="App-link"
-          href="https://cerulean-iguana-839.azurestaticwebsites.net/.auth/login/aad"
-        >
-        Log in with Azure AD
-        </a>
+const Products = withRouter(
+  lazy(() => import(/* webpackChunkName: "products" */ './products/Products')),
+);
+const Discounts = withRouter(
+  lazy(() => import(/* webpackChunkName: "discounts" */ './Discounts')),
+);
 
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderBar />
+        <div className="section columns">
+          <NavBar />
+          <main className="column">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Redirect from="/" exact to="/home" />
+                <Route path="/home" component={Home} />
+                <Route path="/discounts" component={Discounts} />
+                <Route path="/products" component={Products} />
+                <Route exact path="**" component={NotFound} />
+              </Switch>
+            </Suspense>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
